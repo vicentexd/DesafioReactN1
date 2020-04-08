e0b9403a5b7e4b0c26c8320bfbdc11c98df20c11
Vicente Dev <vicente.gabrielv@gmail.com>
2020-04-08 18:36:48 -0300

    Desafio completo

--- src/App.js
+++ src/App.js
@@ -1,29 +1,66 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+
+import api from './services/api';
 
 import "./styles.css";
+import logo from './assets/logo.png';
+
 
 function App() {
+
+  const [ repositories, SetRepository ] = useState([]);
+
+  useEffect(() => {
+    api.get('/repositories').then((res => {
+      SetRepository(res.data)
+    }))
+  }, [])
+
+
+
   async function handleAddRepository() {
-    // TODO
+    const res = await api.post('/repositories',{
+      title: "React Desafio N1",
+      url: "https://github.com/josepholiveira",
+      techs: ["React", "Node.js"],
+    });
+
+    const repository = res.data;
+
+    SetRepository([...repositories, repository]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    api.delete(`/repositories/${id}`).then(() => {
+      const repoIndex = repositories.findIndex(repo => repo.id === id)
+
+      const newRepositories = [...repositories];
+      newRepositories.splice(repoIndex, 1);
+
+      SetRepository(newRepositories);
+      
+    },[])
   }
 
   return (
-    <div>
-      <ul data-testid="repository-list">
-        <li>
-          Repositório 1
+    <div className="container">
+      <div className="box">
+        <img src={logo} alt="Logo"/>
+        <ul data-testid="repository-list">
+          {repositories.map( repository => 
+            <li key={repository.id}>
+            {repository.title}
 
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
-      </ul>
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+          
+          )}
+        </ul>
 
-      <button onClick={handleAddRepository}>Adicionar</button>
+        <button onClick={handleAddRepository}>Adicionar</button>
+      </div>
     </div>
   );
 }
